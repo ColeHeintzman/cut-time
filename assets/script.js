2b89279f99a239a4bcad23df3468a8ba8c94e299
ColeHeintzman <heintzmancoleanthony@gmail.com>
2022-01-16 17:08:10 -0600

    added quiz function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,50 @@
+function quiz(){
+    const output = [];
+    Choices.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+                output.push(
+                    `<div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join('')} </div>` 
+                );
+        }
+    );
+    questionsContainer.innerHTML = output.join('');
+
+}
+
+function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+    Choices.forEach( (currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+                answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+            answerContainers[questionNumber].style.color = 'red';
+         }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+
 const questionsContainer = document.getElementById('questions');
 const submitButton = document.getElementById('submit');
 const resultsContainer = document.getElementById('results');
-
-function quiz(){}
-
-function showResults(){}
-
-quiz();
-
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
+const Choices = [
     {
       question: "Who invented JavaScript?",
       answers: {
@@ -56,5 +90,10 @@ const myQuestions = [
         d: "ESLint"
       },
       correctAnswer: "d"
-    }
-  ];
+}
+];
+
+
+  quiz();
+
+submitButton.addEventListener('click', showResults);
