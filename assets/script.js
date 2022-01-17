230abc6ac2e5a19bfbc7164aa7d18bcf7b60367b
ColeHeintzman <heintzmancoleanthony@gmail.com>
2022-01-16 19:39:10 -0600

    timer added

--- assets/script.js
+++ assets/script.js
@@ -1,99 +1,151 @@
-function quiz(){
+function buildQuiz(){
     const output = [];
-    Choices.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = [];
-            for(letter in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-                output.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join('')} </div>` 
-                );
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+        const answers = [];
+        for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
         }
+        output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+        );
+      }
     );
-    questionsContainer.innerHTML = output.join('');
-
-}
-
-function showResults(){
+      quizContainer.innerHTML = output.join('');
+  }
+  function showResults(){
     const answerContainers = quizContainer.querySelectorAll('.answers');
     let numCorrect = 0;
-    Choices.forEach( (currentQuestion, questionNumber) => {
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
-                answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        else{
-            answerContainers[questionNumber].style.color = 'red';
-         }
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
     });
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
+  }
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
 
-
-const questionsContainer = document.getElementById('questions');
-const submitButton = document.getElementById('submit');
+const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
-const Choices = [
+const submitButton = document.getElementById('submit');
+const myQuestions = [
     {
-      question: "Who invented JavaScript?",
+      question: "Arrays in JavaScript can be used to store:",
       answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "The condition in an if/else statement is enclosed with:",
+      answers: {
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets"
       },
       correctAnswer: "c"
     },
     {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-        question: "Which one of these is a JavaScript package manager?",
+        question: "Commonly used data types DO NOT include:",
         answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
+          a: "strings",
+          b: "booleans",
+          c: "alerts",
+          d: "numbers"
         },
         correctAnswer: "c"
       },
       {
-        question: "Which one of these is a JavaScript package manager?",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
+          a: "JavaScript",
+          b: "terminal/bash",
+          c: "for loops",
+          d: "console.log"
+        },
+        correctAnswer: "d"
+      },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: {
+          a: "commas",
+          b: "curly brackets",
+          c: "quotes",
+          d: "parenthesis"
         },
         correctAnswer: "c"
       },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-}
 ];
 
+var count = 45;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    alert("You're out of time!");
+  }
+}, 1000);
 
-  quiz();
+
+buildQuiz();
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+  
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
 
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+//Coding Quiz Challenge
+//Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!
